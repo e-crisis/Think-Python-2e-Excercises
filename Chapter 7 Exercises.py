bc1f8ed288d68f4d16cb7a21b682aaf764407d6a
Preetika Mondal <preetikamon11@gmail.com>
2018-05-17 15:55:19 +0530

    Update Chapter 7 Exercises.py
    Solution for Exercise 7.1 and 7.2 combined together.

--- Chapter 7 Exercises.py
+++ Chapter 7 Exercises.py
@@ -68,7 +68,7 @@ For example:
 Write a function called eval_loop that iteratively prompts the user, takes the resulting input and
 evaluates it using eval, and prints the result.
 It should continue until the user enters 'done', and then return the value of the last expression it
-evaluated.
+evaluated..
 """
 
 def eval_loop():
